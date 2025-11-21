b473f64e425de1df292c7c62ed77b308ab4b4d6d
Louis Celeyron <115717650+Louis-Celeyron-ETPA@users.noreply.github.com>
2025-11-21 11:56:12 +0100

    Update ToARPG

---
+++ Source/CEL_LOU_ARPG/CEL_LOU_ARPG.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CEL_LOU_ARPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CEL_LOU_ARPG, "CEL_LOU_ARPG" );
